c8e6deded54c47fdd96adbf921f0e4d7c660b97e
kevin-wildcatconnect <kevin@wildcatconnect.com>
2016-08-19 22:08:12 -0400

    New commDelete job.

--- core/web-desktop/controllers/JobController.js
+++ core/web-desktop/controllers/JobController.js
@@ -10,6 +10,10 @@ exports.handleJob = function(req, res) {
                 return exports.newsDelete(req, res);
             case "eventDelete":
                 return exports.eventDelete(req, res);
+            case "commDelete":
+                return exports.commDelete(req, res);
+            case "pollDelete":
+                return exports.pollDelete(req, res);
         }
     } else {
         utils.log("error", "Forbidden access detected from IP address: " + req.connection.remoteAddress, null);
@@ -95,6 +99,100 @@ exports.eventDelete = function(req, res) {
             }
             if (structures.length == 0) {
                 utils.log('info', 'No events.', null);
+                res.send("No objects to delete!");
+            };
+        },
+        error: function() {
+            var rawError = new Error();
+            var x = utils.processError(e, rawError, null);
+            utils.log('error', x.message, {"stack": x.stack, "objects": x.objects});
+            res.send(error.toString());
+        }
+    });
+};
+
+exports.commDelete = function(req, res) {
+    var query = new Parse.Query("CommunityServiceStructure");
+    query.ascending("communityServiceID");
+    query.find({
+        success: function(structures) {
+            for (var i = 0; i < structures.length; i++) {
+                var currentStructure = structures[i];
+                var thisDate = currentStructure.get("endDate");
+                var now = new Date();
+                var one_day=1000*60*60*24;
+                var date1_ms = thisDate.getTime();
+                var date2_ms = now.getTime();
+                var difference_ms = date2_ms - date1_ms;
+                difference_ms = Math.round(difference_ms/one_day);
+                if (difference_ms >= 0) {
+                    currentStructure.destroy({
+                        success: function() {
+                            console.log("Just deleted an object!!!");
+                        },
+                        error: function(error) {
+                            var rawError = new Error();
+                            var x = utils.processError(e, rawError, null);
+                            utils.log('error', x.message, {"stack": x.stack, "objects": x.objects});
+                            res.send(error.toString());
+                        }
+                    });
+                };
+                if (i == structures.length - 1) {
+                    utils.log('info', 'Opportunities successfully deleted.', null);
+                    res.send("SUCCESS");
+                };
+            }
+            if (structures.length == 0) {
+                utils.log('info', 'No opportunities to delete.', null);
+                res.send("No objects to delete!");
+            };
+        },
+        error: function() {
+            var rawError = new Error();
+            var x = utils.processError(e, rawError, null);
+            utils.log('error', x.message, {"stack": x.stack, "objects": x.objects});
+            res.send(error.toString());
+        }
+    });
+};
+
+exports.pollDelete = function(req, res) {
+    var query = new Parse.Query("PollStructure");
+    query.ascending("pollID");
+    query.find({
+        success: function(structures) {
+            for (var i = 0; i < structures.length; i++) {
+                var currentStructure = structures[i];
+                var thisDate = currentStructure.get("createdAt");
+                var now = new Date();
+                var one_day=1000*60*60*24;
+                var date1_ms = thisDate.getTime();
+                var date2_ms = now.getTime();
+                var difference_ms = date2_ms - date1_ms;
+                difference_ms = Math.round(difference_ms/one_day);
+                if (difference_ms >= currentStructure.get("daysActive")) {
+                    currentStructure.save({
+                        "isActive" : 0
+                    }, {
+                        success: function() {
+                            console.log("Just updated an object!!!");
+                        },
+                        error: function(error) {
+                            var rawError = new Error();
+                            var x = utils.processError(e, rawError, null);
+                            utils.log('error', x.message, {"stack": x.stack, "objects": x.objects});
+                            res.send(error.toString());
+                        }
+                    });
+                };
+                if (i == structures.length - 1) {
+                    utils.log('info', 'Polls successfully deleted.', null);
+                    res.send("SUCCESS");
+                };
+            }
+            if (structures.length == 0) {
+                utils.log('info', 'No polls to delete.', null);
                 res.send("No objects to delete!");
             };
         },
